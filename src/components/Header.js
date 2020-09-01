021cd0088d2a2e069222164db281604c43b74229
sanganu <kppkmsanu@gmail.com>
2020-09-01 13:44:42 -0600

    Adding pages and components

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+    return(<header>
+        <h1>Sangeetha Kaliaperumal</h1>
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Portfolio</li>
+            </ul>
+        </nav>
+        </header>)
+}
+export default Header;
---
+++ src/pages/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Landing(){
+    return(<div className="container">
+         <h1>Loren</h1>
+         <img src="" alt="profile picture" />
+         <p>random text</p>
+         
+        </div>)
+}
+
+export default Landing;
