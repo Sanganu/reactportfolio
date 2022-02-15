7c93965f778c963d99fdb98e491c88ba655b2167
sanganu <kppkmsanu@gmail.com>
2022-02-15 10:54:28 -0700

    Updates

---
+++ precache-manifest.d7d43faf0c126bbccab66085954dfbb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c84ab31bf3531ceb1597775ea9c21b9",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "7c1f6f7dc5bc53e49602",
+    "url": "/reactportfolio/static/css/2.12d8507a.chunk.css"
+  },
+  {
+    "revision": "d21d3837f3d28f0b8016",
+    "url": "/reactportfolio/static/css/main.1fc8aab2.chunk.css"
+  },
+  {
+    "revision": "7c1f6f7dc5bc53e49602",
+    "url": "/reactportfolio/static/js/2.a25a1677.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/reactportfolio/static/js/2.a25a1677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21d3837f3d28f0b8016",
+    "url": "/reactportfolio/static/js/main.225655a0.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  }
+]);
