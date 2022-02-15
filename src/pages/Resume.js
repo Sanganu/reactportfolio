8520587ed8b5544a9a4bb94700d88016149597a3
sanganu <kppkmsanu@gmail.com>
2022-02-15 11:21:24 -0700

    Add images

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,7 +5,7 @@ const Resume = () => {
     return(<main className="main-content">
     <h3 className="text-center page-header">Resume</h3>
     <Container>
-    <iframe title="resume" className="m-3 p-3" src="/resource/ResumeTemplate.pdf" 
+    <iframe title="resume" className="m-3 p-3" src={process.env.PUBLIC_URL+"/resource/ResumeTemplate.pdf"} 
     width="740" height="947" frameBorder="5" marginHeight="2" marginWwidth="4">
     Loading...</iframe>
     </Container>
